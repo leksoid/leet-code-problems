87db7484bc0f28dbe351f8221c6955980bbe68c0
leksoid <aliaksei.taran@gmail.com>
2020-01-28 12:24:59 -0500

    Add desc

--- difficulty/easy/two-sum-solution.py
+++ difficulty/easy/two-sum-solution.py
@@ -3,14 +3,10 @@
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
 
-
+# brute force - time complexity O(n^2)
 def two_sum(list, target):
     for f_index, f_value in enumerate(list):
         for s_index, s_value in enumerate(list[f_index+1:]):
             if (f_value + s_value) == target:
                 return [f_index, list.index(s_value)]
     return None
-
-result = two_sum([2,7,11,15], 9)
-
-print(result)
